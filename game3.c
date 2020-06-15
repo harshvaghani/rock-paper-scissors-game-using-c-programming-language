b449aa7f11fcfda8ab4277c2ca2927c3d1106e54
Harsh Vaghani <harshvaghani02@gmail.com>
2020-06-15 14:27:17 +0530

    Update game3.c

--- game3.c
+++ game3.c
@@ -6,6 +6,85 @@
 //     srand(time(NULL));
 //     return ((rand() % 3) + 1);
 // }
+int charselect(char playerselect, char compselect);
+int main()
+{
+    while (1)
+    {
+        char game[3] = {'r', 'p', 's'};
+        char playerselect, compselect;
+        int num;
+        printf("enter a number\n1 for Rock\n2 for Paper\n3 for Scissors\n: ");
+        scanf("%d", &num);
+        playerselect = game[num - 1];
+        if (playerselect == 'r')
+        {
+            printf("You selected rock\n");
+        }
+
+        else if (playerselect == 'p')
+        {
+            printf("You selected paper\n");
+        }
+
+        else if (playerselect == 's')
+        {
+            printf("You selected scissors\n");
+        }
+
+        int temp = (rand() % 3) + 1;
+        int playerscore = 0;
+        int compscore = 0;
+
+        compselect = game[temp - 1];
+        if (compselect == 'r')
+        {
+            printf("Computer selected rock\n");
+        }
+
+        else if (compselect == 'p')
+        {
+            printf("Computer selected paper\n");
+        }
+
+        else if (compselect == 's')
+        {
+            printf("Computer selected scissors\n");
+        }
+
+        charselect(playerselect, compselect);
+        if (charselect(playerselect, compselect) == 0)
+        {
+            compscore = 0;
+            playerscore = 0;
+            charselect(playerselect, compselect);
+        }
+        else if (charselect(playerselect, compselect) == 1)
+        {
+            playerscore = 1;
+            charselect(playerselect, compselect);
+        }
+        else if (charselect(playerselect, compselect) == -1)
+        {
+            compscore = 1;
+            charselect(playerselect, compselect);
+        }
+        printf("Your score : %d\n", playerscore);
+        printf("CPU's score : %d\n", compscore);
+        if (playerscore == 1)
+        {
+            printf("You won\n");
+        }
+        else if (compscore == 1)
+        {
+            printf("COMPUTER won\n");
+        }
+        else
+        {
+            printf("DRAW\n");
+        }
+    }
+}
 int charselect(char playerselect, char compselect)
 {
     if (playerselect == compselect)
@@ -17,83 +96,25 @@ int charselect(char playerselect, char compselect)
     if (playerselect == 'r' && compselect == 'p') //if both true then and then we get into this condition otherwise we can't
     {
         return -1;
-        printf("You selected Rock\n");
-        printf("computer selected paper\n");
     }
     if (playerselect == 'r' && compselect == 's')
     {
         return 1;
-        printf("You selected Rock\n");
-        printf("computer selected scissors\n");
     }
     if (playerselect == 'p' && compselect == 'r')
     {
         return 1;
-        printf("computer selected rock\n");
-        printf("You selected Paper\n");
     }
     if (playerselect == 'p' && compselect == 's')
     {
         return -1;
-        printf("You selected Paper\n");
-        printf("computer selected scissors\n");
     }
     if (playerselect == 's' && compselect == 'r')
     {
         return -1;
-        printf("You selected scissors\n");
-        printf("computer selected rock\n");
     }
     if (playerselect == 's' && compselect == 'p')
     {
         return 1;
-        printf("You selected scissors\n");
-        printf("You selected Paper\n");
-    }
-}
-int main2()
-{
-    char game[3] = {'r', 'p', 's'};
-    char playerselect, compselect;
-    int num;
-    printf("enter a number\n1 for Rock\n2 for Paper\n3 for Scissors\n : ");
-    scanf("%d", &num);
-    playerselect = game[num - 1];
-    printf("%c\n", playerselect);
-    int temp = (rand() % 3) + 1;
-    int playerscore = 0;
-    int compscore = 0;
-
-    compselect = game[temp - 1];
-    charselect(playerselect, compselect);
-    if (charselect(playerselect, compselect) == 0)
-    {
-        compscore = 0;
-        playerscore = 0;
-        charselect(playerselect, compselect);
-    }
-    else if (charselect(playerselect, compselect) == 1)
-    {
-        playerscore = 1;
-        charselect(playerselect, compselect);
-    }
-    else if (charselect(playerselect, compselect) == -1)
-    {
-        compscore = 1;
-        charselect(playerselect, compselect);
-    }
-    printf("Your score : %d\n", playerscore);
-    printf("CPU's score : %d\n", compscore);
-    if (playerscore == 1)
-    {
-        printf("You won\n");
-    }
-    else if (compscore == 1)
-    {
-        printf("Compouter won\n");
-    }
-    else
-    {
-        printf("Draw\n");
     }
 }
